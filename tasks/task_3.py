aa4a029279e036ab626c298432dc4b612a76e42b
JK <LLL@LLL.ru>
2021-01-11 16:10:43 +0300

    Task_1

--- tasks/task_3.py
+++
@@ -1,8 +0,0 @@
-import os
-fname = input('Введите названия для файла: ')
-file = open(fname + '.txt', "w")
-file1 = file.write(input('Введите свой текст: '))
-file.close()
-file = open(fname + '.txt')
-file.close()
-os.remove(fname + '.txt')
