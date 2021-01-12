05dee7eef6326ebab62c6707c0d30b1947d8bdc7
JK <LLL@LLL.ru>
2021-01-12 12:03:06 +0300

    Task_2

--- tasks/task_1.py
+++
@@ -1,6 +0,0 @@
-word = input('Введите своё слово: ')
-word = word.replace(' ','').replace('.','').replace(',','').lower()
-if word == word [::-1]:
-    print('Palindrome')
-else:
-    print('Oh no!')
