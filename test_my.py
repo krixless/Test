2d16bd99ff040a8550fe1d0c3f559e6e412fda9c
Eugene <krixless@mail.ru>
2021-06-18 10:10:16 +0300

    allure added

--- test_my.py
+++ test_my.py
@@ -3,12 +3,16 @@ import pytest
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import allure
 
+
+@allure.title('test_google_search')
 def test_google_search():
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    driver.get('https://mail.ru/')
-    input_field = driver.find_element_by_xpath("//input[@id='q']")
-    input_field.send_keys('python')
-    input_field.send_keys(Keys.ENTER)
-    time.sleep(5)
-    driver.close()
+    with allure.step('Starting'):
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        driver.get('https://mail.ru/')
+        input_field = driver.find_element_by_xpath("//input[@id='q']")
+        input_field.send_keys('python')
+        input_field.send_keys(Keys.ENTER)
+    with allure.step('Closing'):
+        driver.close()
